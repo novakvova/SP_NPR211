5775c54772975e008bea0f28b7f6d6130c5fcfa7
novakvova <novakvova@gmail.com>
2024-04-21 09:48:08 +0300

    Show list users

---
+++ WpfAppThread/UsersWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppThread
+{
+    /// <summary>
+    /// Interaction logic for UsersWindow.xaml
+    /// </summary>
+    public partial class UsersWindow : Window
+    {
+        public UsersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Hello Peter");
+            MyDataContext myDataContext = new MyDataContext();
+            var users = myDataContext.Users.ToList();
+            dgSimple.ItemsSource = users;
+        }
+    }
+}
